b1890db99c8b2eb9f60efc878ee4c66b73118af6
O. Drachuk <ksenorin@gmail.com>
2020-12-04 15:02:57 -0600

    Fixes singup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,8 @@ class SessionsController < ApplicationController
 
   def create
     @customer = Customer.find_by(email: params[:email])
-    if @customer && @customer.authenticate(params[:password])
+    if @customer && @customer.authenticate(params[:password_digest])
+        reset_session
         session[:customer_id] = @customer.id
         redirect_to root_path
     else
