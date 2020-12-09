b72bed6a76be0bb6331bb3d09a09bbe895ef8d4a
O. Drachuk <ksenorin@gmail.com>
2020-12-08 23:18:17 -0600

    Finishes shopping cart

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -3,40 +3,67 @@ class CartController < ApplicationController
 
   def index
     @products = session[:cart]
-    @total = 0
-    @products.each do |product|
-      @total = @total + product["quantity"] * product["price"].to_f
+    @subtotal = get_subtotal(@products)
+    @taxes = get_taxes(@products)
+    @total = @subtotal + @taxes
+  end
+
+  def get_product_total(product)
+    product["quantity"] * product["price"].to_f
+  end
+
+  def get_subtotal(products)
+    subtotal = 0
+    products.each do |product|
+      subtotal = subtotal + get_product_total(product)
     end
-    @total
+    subtotal
+  end
+
+  def get_taxes(products)
+    customer = Customer.find_by(id: session[:customer_id])
+    province = Province.find_by(id: customer.province_id)
+    gst_hst = province.gst_hst.to_f
+    pst = province.pst.to_f
+    taxes = get_subtotal(products) * gst_hst + get_subtotal(products) * pst
   end
 
   def remove_from_cart
-    id = params[:id].to_i
-    session[:cart].delete(:id)
-    redirect_to :controller => 'cart', :action => 'index'
+    cart = session[:cart]
+    item = cart.find { |item| item["id"] == params[:id].to_i }
+    cart.delete(item)
+    subtotal = get_subtotal(cart)
+    taxes = get_taxes(cart)
+    total = subtotal + taxes
+    render json: { id: params[:id], subtotal: subtotal, taxes: taxes, total: total }
   end
 
   def increment(item)
-    new_quantity = item["quantity"] + 1
-    new_quantity
+    item["quantity"] + 1
   end
 
   def decrement(item)
-    new_quantity = item["quantity"] - 1
-    new_quantity
+    item["quantity"] - 1
   end
 
   def qty
-    @cart = session[:cart]
-    @item = @cart.find { |item| item["id"] == params[:id].to_i }
-
+    cart = session[:cart]
+    item = cart.find { |item| item["id"] == params[:id].to_i }
     if request.post?
-      new_quantity = increment(@item)
-    elsif request.delete?
-      new_quantity = decrement(@item)
+      if params[:operation] == "plus"
+        item["quantity"] = item["quantity"] + 1
+      elsif params[:operation] == "minus"
+        item["quantity"] = item["quantity"] - 1
+        if item["quantity"] == 0
+          cart.delete(item)
+        end
+      end
     end
-
-    return render json: { new_quantity: new_quantity}
+    subtotal = get_subtotal(cart)
+    taxes = get_taxes(cart)
+    total = subtotal + taxes
+    render json: { id: params[:id], quantity: item["quantity"],
+                   product_total: get_product_total(item), subtotal: subtotal, taxes: taxes, total: total }
   end
 
 end
--- app/javascript/packs/cart.js
+++ app/javascript/packs/cart.js
@@ -1,4 +1,13 @@
 document.body.addEventListener('ajax:success', function (event, data, status, xhr) {
-    var response = event.detail[0].new_quantity;
-    alert("Response is => " + response)
+    var response = event.detail[0];
+    if (response.quantity > 0) {
+        document.querySelector(".product#id_" + response.id + ">td.quantity").innerHTML = response.quantity;
+        document.querySelector(".product#id_" + response.id + ">td.product_total").innerHTML = response.product_total;
+    } else {
+        const row = document.querySelector(".product#id_" + response.id);
+        row.parentNode.removeChild(row);
+    }
+    document.querySelector(".subtotal").innerHTML = response.subtotal;
+    document.querySelector(".taxes").innerHTML = response.taxes;
+    document.querySelector(".total").innerHTML = response.total;
 })
