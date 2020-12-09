e3ae597661e571b72cb8ab31a1f462141ac17668
O. Drachuk <ksenorin@gmail.com>
2020-12-09 12:35:39 -0600

    Fixes problems with checkout

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -10,11 +10,11 @@ class CheckoutController < ApplicationController
     @session = Stripe::Checkout::Session.create(
       payment_method_types: ['card'],
       line_items: [{
-        name: 'Estimated total'
-        amount: @total,
-        currency: 'cad'
-      }]
-
+                     name: 'Estimated total',
+                     amount: (@total.to_f*100).to_i,
+                     currency: 'cad',
+                     quantity: 1
+                   }],
       success_url: checkout_success_url + '?session_id={CHECKOUT_SESSION_ID}',
       cancel_url: checkout_cancel_url
     )
